2b67ff26677b3ca1c52b79079c091b68d65f3415
xiaojin <1499411376@qq.com>
2019-10-22 19:51:42 +0800

    搭建了生产环境

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,8 @@
-const autoprefixer = require("autoprefixer")
+
+const autoprefixer = require("autoprefixer");
 module.exports={
   plugins:[
-  autoprefixer()
+    autoprefixer()
   ]
 }
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import "@babel/polyfill"
-import "frame/frame.js"
-import "bussiness/about.js"
-import "bussiness/crouse.js"
-import "bussiness/home.js"
-import "bussiness/team.js"
-import "bussiness/work.js"
+import "./frame/frame"
+import "./bussiness/about"
+import "./bussiness/crouse"
+import "./bussiness/home"
+import "./bussiness/team"
+import "./bussiness/work"
 
+setTimeout(()=> {
+    console.log(Object.assign({},{name:"xiaojin"},{age:19}))
+},1000)
