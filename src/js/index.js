e169b1995bc60dd21257c2c0ff50236ab93b2345
xiaojin <1499411376@qq.com>
2019-10-22 15:30:41 +0800

    初始化提交

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import "@babel/polyfill"
+import "frame/frame.js"
+import "bussiness/about.js"
+import "bussiness/crouse.js"
+import "bussiness/home.js"
+import "bussiness/team.js"
+import "bussiness/work.js"
+
